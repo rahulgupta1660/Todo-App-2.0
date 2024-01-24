c0a25e8587ef3793bc9c09e9045d66d88a1a45a1
RAHUL GUPTA <contact.rahul1660@gmail.com>
2024-01-24 18:31:15 +0530

    changes

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ let ul = document.querySelector("ul");
 btn.addEventListener("click", () => {
   if (input.value != "") {
     let li = document.createElement("li");
-    let close = document.createElement("button");
     li.innerText = input.value;
-    close.innerText = "X";
+    let close = document.createElement("button");
     close.classList.add("close");
-    ul.appendChild(li);
+    close.innerText = "X";
     li.appendChild(close);
+    ul.appendChild(li);
   }
   input.value = "";
 });
